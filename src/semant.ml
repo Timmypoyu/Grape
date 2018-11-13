0058ad43c4b42498acd86feaeeebb0d64c86de60
Tim <pw2440@columbia.edu>
2018-11-13 17:59:45 -0500

    feat: add typ

--- src/semant.ml
+++ src/semant.ml
@@ -95,7 +95,12 @@ let check (globals, functions) =
         IntLit  l  -> (Int, SIntLit l)
       | FloatLit l -> (Float, SFloatLit l)
       | BoolLit l  -> (Bool, SBoolLit l)
-      | StrLit s   -> (string, SStrLit s)
+      | StrLit s   -> (Str, SStrLit s)
+      | NodeLit s -> (Node, SNodeLit s)
+      | ListLit s -> (List, SListLit s) 
+      | DictLit s -> (Dict, SDictLit s)
+      | DirEdgeLit s -> (Edge, SDirEdgeLit s)
+      | EdgeLit s -> (Edge, EdgeLit s) 
       | Noexpr     -> (Void, SNoexpr)
       | Id s       -> (type_of_identifier s, SId s)
       | Assign(var, e) as ex -> 
