a269ba275fed3bd4da807c1521d25e8dbc4057b2
HyunBin Yoo <hy2506@columbia.edu>
2018-12-03 03:42:11 -0500

    feat: implemented leaves, adjacent in C

--- src/grape.c
+++ src/grape.c
@@ -259,12 +259,14 @@ struct List *list_remove(struct List *list, void *y) {
 */
 
 struct Graph *GraphInit(struct Graph *glist) {
+	
 	struct Graph *graph = (struct Graph *)malloc(sizeof(struct Graph));
 	initList(graph->nodes);
 	return graph;
 }
 
 struct Node *GraphCreateNode(void *inputData, void *weight, struct Node *inputTo, struct Node *inputFrom) {
+	
 	struct Node *node = (struct Node *)malloc(sizeof(struct Node));
 	struct List *elist = (struct List *)malloc(sizeof(struct List));
 	struct Edge *edge = (struct Edge *)malloc(sizeof(struct Edge));
@@ -280,6 +282,7 @@ struct Node *GraphCreateNode(void *inputData, void *weight, struct Node *inputTo
 
 // when the node already exists
 void GraphAddEdge(struct Graph *graph, void *weight, struct Node *inputTo, struct Node *inputFrom, void *value) {
+	
 	struct ListNode *node = (graph->nodes)->head;
     while (node) {
 		if (node->data == value) { // find the same node and add that edge
@@ -298,11 +301,13 @@ void GraphAddEdge(struct Graph *graph, void *weight, struct Node *inputTo, struc
 }
 	
 struct Graph *GraphAddNode(struct Graph *graph, struct Node *node) {
+	
 	addFront(graph->nodes, node);
 	return graph;
 }
 
 int GraphSize(struct Graph *graph) {
+	
 	int graphSize = size(graph->nodes);	
 	return graphSize;
 
@@ -315,12 +320,29 @@ struct GraphNode *GraphRoot(struct Graph *graph) {
 }
 */
 
-struct List *GraphLeaves(struct Graph graph) {
+struct List *GraphLeaves(struct Graph *graph) {
 	
-	return NULL;
+	struct List *leavesList = (struct List *)malloc(sizeof(struct List));
+	struct ListNode *target = (graph->nodes)->head;
+	while (target) {
+		struct Node *targetGraph = (struct Node *)(target->data);
+		struct List *edges = targetGraph->edges;
+		struct ListNode *tedge = edges->head;
+		while (tedge) {
+			struct Node *anode = ((struct Edge *)(tedge->data))->to;
+			if (anode == NULL) {
+				addFront(leavesList, anode);
+				tedge = tedge->next;	
+			}
+		target = target->next;
+
+		}
+	}	
+	return leavesList;
 }
 
 struct List *GraphAdjacent(struct Graph *graph, struct Node *node) {
+	
 	struct List *adjacentList = (struct List *)malloc(sizeof(struct List));
 	struct ListNode *target = (graph->nodes)->head;
 	while (target) {
