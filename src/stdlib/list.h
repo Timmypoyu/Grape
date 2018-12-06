4a35830d348072f72fe6d3283273dea17544bbee
HyunBin Yoo <hy2506@columbia.edu>
2018-12-06 18:36:00 -0500

    feat: list implementation

--- src/stdlib/list.h
+++ src/stdlib/list.h
@@ -5,15 +5,31 @@
 #include "types.h"
 
 int size(struct List *list);
-void initList(struct List *list);
-void *popFront(struct List *list);
+
+void init_list();
+
+void push_list(struct List *list, void *data);
+
+void push_front_list(struct List *list, void *data);
+
+void pop_front_list(struct List *list);
+
+void pop_list(struct List *list);
+
 int isEmptyList(struct List *list);
+
 void reverseList(struct List *list);
+
 struct List *copy(struct List *list);
+
 void removeAllNodes(struct List *list);
+
 bool isEqual(struct ListNode *a, struct ListNode *b);
+
 struct List *list_remove(struct List *list, void *y);
+
 struct List *insert(int x, struct List *list, void *y);
-struct ListNode *addBack(struct List *list, void *data);
-struct ListNode *addFront(struct List *list, void *data);
+
+
 struct ListNode *addAfter(struct List *list, struct ListNode *prevNode, void *data);
+
